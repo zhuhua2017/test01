e331b6fcb48a07d302c1c6f612a16e05e81bdc21
zhuhua2017 <877380784@qq.com>
2017-06-13 16:14:59 +0800

    1111

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -19,7 +19,7 @@ namespace WindowsFormsApplication1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("大神你好，大神加油！！！");
+            MessageBox.Show(" 大神你好，大神加油！！！");
         }
     }
 }
